dc1d00656d234b700fe33505ad592b73a0974b77
IsabelCollado <icolladoherrera@gmail.com>
2024-04-18 12:08:34 +0200

    ticket2 Add functionality to wink

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const faceEl = document.querySelector('.js-btn');
+const btnEl = document.querySelector('.js-btn');
+
+function handleButtonClick(event) {
+  event.preventDefault();
+  if (event.type === 'click') {
+    faceEl.innerHTML = ';)';
+  } else {
+    faceEl.innerHTML = ':)';
+  }
+}
+btnEl.addEventListener('click', handleButtonClick);
